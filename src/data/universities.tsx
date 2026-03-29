637348cf990e1738d8ba1fd2231319ba273faae4
SANAULLAHKHANSATTI <sanaullahsatti62@gmail.com>
2026-03-29 18:31:50 +0500

    Add two new universities for featured university

--- src/data/universities.tsx
+++ src/data/universities.tsx
@@ -24,6 +24,32 @@ export const universitys = [
       "Listed in WDOMS | Recognized by ECFMG",
       "Very Reasonable Fees | Significantly Lower Than EU Programs"
     ]
+  },
+    {
+    name: "Changsha Medical University",
+    location: "Hunan Province, China",
+    image: "/universities/changsha.jpeg",
+    duration: "6 Years",
+    features: [
+      "ESI Top 1% Globally | High-Impact Research University",
+      "36 Affiliated Hospitals | Advanced Clinical Training",
+      "Globally Recognized | USA, UK, Pakistan, KSA, UAE, Qatar, Oman, Bangladesh",
+      "Listed in WDOMS | Recognized by ECFMG",
+      "Very Reasonable Fees | Significantly Lower Than EU Programs"
+    ]
+  },
+    {
+    name: "Yangtze Medical University",
+    location: "Jiangsu Province, China",
+    image: "/universities/yangtze.jpeg",
+    duration: "6 Years",
+    features: [
+      "ONE OF THE LOWEST-COST MBBS PROGRAMS IN CHINA",
+      "36 Affiliated Hospitals | Advanced Clinical Training",
+      "Halal food | 4+ Pakistani restaurants nearby | 1100+ international students | Enrolling since 2003 – multiple batches graduated",
+      "Listed in WDOMS & WHO | Recognized by ECFMG",
+      "Very Reasonable Fees | Significantly Lower Than EU Programs"
+    ]
   }
 
 ];
